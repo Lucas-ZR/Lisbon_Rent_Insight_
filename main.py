3ddc5a187a59e6df5f71e511c443d2d68d72bdc4
lucaszulchner-dt <lucas.rios@devoteam.com>
2026-05-02 22:08:40 +0100

    testing and refactor init

--- main.py
+++ main.py
@@ -4,3 +4,12 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+
+#driver = uc.Chrome()
+#for url in urls:
+    #get url, w pagination
+    #check minor urls
+    #commit sucessess
+    #retry fails
+    #proxy
