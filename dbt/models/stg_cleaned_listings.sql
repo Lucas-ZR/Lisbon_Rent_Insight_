01cee268cfe4860de935ce90dc68424d2e18e6fc
Lucas-ZR <lucas.zulchner@gmail.com>
2026-05-29 22:29:16 +0100

    dbt config changes

--- dbt/models/stg_cleaned_listings.sql
+++ dbt/models/stg_cleaned_listings.sql
@@ -1,7 +1,7 @@
 WITH base AS (
     SELECT
         *,
-        ARRAY_TO_STRING(extra, ' ') AS extra_str
+        ARRAY_TO_STRING(detail, ' ') AS detail_str
     FROM {{ source('raw', 'raw_listings') }}
 )
 
@@ -9,11 +9,11 @@ WITH base AS (
 SELECT
     id                                                           AS listing_id,
     CAST(REGEXP_REPLACE(price, '[^0-9]', '', 'g') AS INTEGER)    AS price,
-    CAST(REGEXP_EXTRACT(extra_str, 'T(\d+)', 1) AS INTEGER)      AS rooms,
-    CAST(REGEXP_EXTRACT(extra_str, '(\d+) m²', 1) AS INTEGER)    AS sqm,
-    CONTAINS(extra_str, 'Garagem')                               AS has_garage,
-    CONTAINS(extra_str, 'Cave')                                  AS is_cave,
-    CONTAINS(extra_str, 'com elevador')                          AS has_lift,
+    CAST(REGEXP_EXTRACT(detail_str, 'T(\d+)', 1) AS INTEGER)     AS rooms,
+    CAST(REGEXP_EXTRACT(detail_str, '(\d+) m²', 1) AS INTEGER)   AS sqm,
+    CONTAINS(detail_str, 'Garagem')                              AS has_garage,
+    CONTAINS(detail_str, 'Cave')                                 AS is_cave,
+    CONTAINS(detail_str, 'com elevador')                         AS has_lift,
     freguesia                                                    AS freguesia,
     EXTRACT(year FROM scraped_at)                                AS listing_year,
     EXTRACT(month FROM scraped_at)                               AS listing_month,
